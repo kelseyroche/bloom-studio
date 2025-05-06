f40b47568ac8a534643896f5e966ff1e7fb15867
Kelsey Roche <roche.kelsey.24@gmail.com>
2025-05-06 13:29:27 -0400

    work w us button fxn

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 
-
 // import React from 'react';
 // import arrow from '../../assets/arrow.png';
+// import './Hero.css'
 // import content1 from '../../assets/content1.png';
 // import content2 from '../../assets/content2.png';
 // import content3 from '../../assets/content3.png';
@@ -10,26 +10,26 @@
 // const Hero = () => {
 //   return (
 //     <section id="hero" className="hero-section">
-//     <div className="hero-content">
-//       <div className="hero-top-row">
-//         <div className="hero-text-left">
-//           <h1 className="hero-main-text">Design</h1>
-//           <h1 className="hero-sub-text">for you</h1>
+//       <div className="hero-content">
+//         <div className="hero-top-row">
+//           <div className="hero-text-left">
+//             <h1 className="hero-main-text">Design</h1>
+//             <h1 className="hero-sub-text">for you</h1>
+//           </div>
+//           <div className="hero-cta-right">
+//             <img src={arrow} alt="Arrow" className="hero-arrow" />
+//             <button className="hero-button">Work with us</button>
+//           </div>
 //         </div>
-//         <div className="hero-cta-right">
-//           <img src={arrow} alt="Arrow" className="hero-arrow" />
-//           <button className="hero-button">Work with us</button>
+
+//         <div className="hero-images-row">
+//           <img src={content1} alt="Content 1" className="hero-image" />
+//           <img src={content2} alt="Content 2" className="hero-image" />
+//           <img src={content3} alt="Content 3" className="hero-image" />
+//           <img src={content4} alt="Content 4" className="hero-image" />
 //         </div>
 //       </div>
-  
-//       <div className="hero-images-row">
-//         <img src={content1} alt="Content 1" className="hero-image" />
-//         <img src={content2} alt="Content 2" className="hero-image" />
-//         <img src={content3} alt="Content 3" className="hero-image" />
-//         <img src={content4} alt="Content 4" className="hero-image" />
-//       </div>
-//     </div>
-//   </section>
+//     </section>
 //   );
 // };
 
@@ -54,7 +54,9 @@ const Hero = () => {
           </div>
           <div className="hero-cta-right">
             <img src={arrow} alt="Arrow" className="hero-arrow" />
-            <button className="hero-button">Work with us</button>
+            <a href="#contact">
+              <button className="hero-button">Work with us</button>
+            </a>
           </div>
         </div>
 
